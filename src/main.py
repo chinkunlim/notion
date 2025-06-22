50a60096256356c99ecd3f2ec7f2693126cb6692
chinkunlim <411383025@gms.ndhu.edu.tw>
2025-06-22 22:34:02 +0800

    Project Initialization

---
+++ src/main.py
@@ -0,0 +1,34 @@
+# scr/main.py
+# import libraries
+import logging
+import time
+from pathlib import Path
+from rich.console import Console
+from rich.logging import RichHandler
+
+# import modules
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+# print(f"PROJECT_ROOT: {PROJECT_ROOT}")
+
+def setup_logging():
+    log_dir = PROJECT_ROOT / "logs"
+    log_dir.mkdir(exist_ok=True)
+    log_file = log_dir / "app.log"
+
+    logging.basicConfig(
+        level="INFO",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(log_file, mode="a", encoding="utf-8"),
+            RichHandler(rich_tracebacks=True)
+        ]
+    )
+
+def main():
+    setup_logging()
+    console = Console()
+    console.print(PROJECT_ROOT)
+
+if __name__ == "__main__":
+    main()
